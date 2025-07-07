6358b25ff88e12b0b2b69ee66959598b45e1ffcc
Alex.Araújo <125103074+alexsouza10@users.noreply.github.com>
2025-07-07 20:37:58 -0300

    Add vue-chartjs and chart.js dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -43,7 +43,7 @@ export default defineNuxtConfig({
   },
 
   nitro: {
-    preset: 'vercel-static',
+    preset: 'vercel',
     prerender: {
       crawlLinks: true,
       routes: ['/', '/404.html'],
